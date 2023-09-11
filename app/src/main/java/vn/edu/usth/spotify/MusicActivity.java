1df4bba449d62441033519046bf31bd596b3d395
Spring317 <daoxuanquyhp@gmail.com>
2023-09-11 13:32:31 +0700

    MAD001 - Create and setup MusicActivity.java

---
+++ app/src/main/java/vn/edu/usth/spotify/MusicActivity.java
@@ -0,0 +1,43 @@
+package vn.edu.usth.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MusicActivity extends AppCompatActivity {
+    private static final String TAG = "Spotify says: ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+        Log.i(TAG, "onCreate: Success");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: Start successful");
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPause: Pausing");
+    }
+    @Override
+    protected void  onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume: Resuming");
+    }
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG, "onStop: Stop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: Destroy");
+    }
+
+}
