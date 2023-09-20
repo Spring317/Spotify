a87bfa11f75920a4689ff74c46a25e0f9dfd1893
BI12-403NguyenVanThang <thangnv.bi12-403@st.usth.edu.vn>
2023-09-20 14:43:26 +0700

    MAD012 - Initialize LibraryFragment

---
+++ app/src/main/java/vn/edu/usth/spotify/LibraryFragment.java
@@ -0,0 +1,92 @@
+package vn.edu.usth.spotify;
+import android.view.View;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LibraryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LibraryFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private ImageButton button1;
+    private ImageButton button2;
+    public LibraryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LibraryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LibraryFragment newInstance(String param1, String param2) {
+        LibraryFragment fragment = new LibraryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_library, container, false);
+
+
+        button1 = view.findViewById(R.id.menu);
+        button2 = view.findViewById(R.id.list);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setVisibility(View.INVISIBLE);
+                button2.setVisibility(View.VISIBLE);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button2.setVisibility(View.INVISIBLE);
+                button1.setVisibility(View.VISIBLE);
+            }
+        });
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+
+
+}
+
