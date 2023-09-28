b21fb117f10bc63f95d2fc68a83627159f844076
BI12-403NguyenVanThang <thangnv.bi12-403@st.usth.edu.vn>
2023-09-28 10:35:57 +0700

    MAD019 - Initialize LibraryRecyclerView

---
+++ app/src/main/java/vn/edu/usth/spotify/LibraryAdapter.java
@@ -0,0 +1,51 @@
+package vn.edu.usth.spotify;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class LibraryAdapter extends RecyclerView.Adapter<LibraryViewHolder> {
+    private Context context;
+    List<LibraryItem> items;
+
+    public LibraryAdapter(Context context, List<LibraryItem> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public LibraryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new LibraryViewHolder(LayoutInflater.from(context).inflate(R.layout.library_activity,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LibraryViewHolder holder, int position) {
+        holder.nameView.setText(items.get(position).getLibrary_name());
+        holder.typeView.setText(items.get(position).getLibrary_type());
+        holder.imageView.setImageResource(items.get(position).getLibrary_image_view());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the item click event here
+                // You can perform any desired action for the clicked item
+                // For example, you can launch a new activity, show a dialog, etc.
+                Log.i("buttonclick", "Clicked on button");
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/spotify/LibraryItem.java
@@ -0,0 +1,38 @@
+package vn.edu.usth.spotify;
+
+public class LibraryItem {
+
+    String library_name;
+    String library_type;
+    int library_image_view;
+
+    public LibraryItem(String library_name, String library_type, int library_image_view) {
+        this.library_name = library_name;
+        this.library_type = library_type;
+        this.library_image_view = library_image_view;
+    }
+
+    public String getLibrary_name() {
+        return library_name;
+    }
+
+    public void setLibrary_name(String library_name) {
+        this.library_name = library_name;
+    }
+
+    public String getLibrary_type() {
+        return library_type;
+    }
+
+    public void setLibrary_type(String library_type) {
+        this.library_type = library_type;
+    }
+
+    public int getLibrary_image_view() {
+        return library_image_view;
+    }
+
+    public void setLibrary_image_view(int library_image_view) {
+        this.library_image_view = library_image_view;
+    }
+}
