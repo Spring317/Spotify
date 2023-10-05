35b275bd856187127ecdc43ec26d2c10fb5e8923
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2023-10-05 23:51:56 +0700

    MAD026 - Create a callback mechanism to handle the completion of an API call

--- app/src/main/java/vn/edu/usth/spotify/Callback.java
+++ app/src/main/java/vn/edu/usth/spotify/Callback.java
@@ -1,2 +1,15 @@
-package vn.edu.usth.spotify;public interface Callback {
+package vn.edu.usth.spotify;
+
+import org.json.JSONObject;
+
+/**
+ * This interface defines a callback mechanism to handle the completion of an API call.
+ */
+public interface Callback {
+    /**
+     * Called when an API call is complete and returns a JSON object as a result.
+     *
+     * @param jsonObject The JSON object returned by the API call.
+     */
+    void onAPICallComplete(JSONObject jsonObject);
 }
