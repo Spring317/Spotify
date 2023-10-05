b30ace051dbf91d4fe43f8be37bdc135a40adfac
Nguyen Duy Anh Tung <tungnda.bi12-484@st.usth.edu.vn>
2023-10-05 12:51:29 +0700

    MAD018 - Added search engine with fragment handled

---
+++ app/src/main/java/vn/edu/usth/spotify/SearchLayout.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.spotify;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchLayout extends Fragment {
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        MusicActivity musicActivity = (MusicActivity) getActivity();
+        if (musicActivity !=null){
+            musicActivity.restoreViewPager();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.search_layout, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.searchlistrecyclerview);
+        List<SearchFragmentData> datas = new ArrayList<>();
+
+        TextView Cancelbtn = view.findViewById(R.id.Cancelbtn);
+        Cancelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("SearchExtend", "Click on Cancel button");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.remove(SearchLayout.this);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return view;
+    }
+}
